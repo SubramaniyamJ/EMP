ef77e032646b4aef9f620eafaa60e9679c894d6f
Sridharan-AR4 <sridhar47914@gmail.com>
2024-08-06 19:42:35 +0530

    manage students and departments components added

--- emp-frontend/src/components/Chatbot.jsx
+++ emp-frontend/src/components/Chatbot.jsx
@@ -26,7 +26,7 @@ export default Chatbot;
 // import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 // import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-// const API_KEY = "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";
+// const API_KEY = "";
 // // "Explain things like you would to a 10 year old learning how to code."
 // const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
 //   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
